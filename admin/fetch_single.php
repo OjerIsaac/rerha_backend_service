df02bf15b0df3591cbfb1183f37627ef0a55bad7
Ojer Isaac <isaacojerumu@gmail.com>
2023-02-23 23:17:42 +0100

    chores: removed jwt from fetch single

--- admin/fetch_single.php
+++ admin/fetch_single.php
@@ -11,15 +11,6 @@ $__DotEnvironment = new DotEnvironment(realpath("./.env"));
 
 require_once "../classes/classes.php";
 
-use \Firebase\JWT\JWT;
-use \Firebase\JWT\Key;
-
-require '../Firebase-JWT/src/JWT.php';
-require '../Firebase-JWT/src/Key.php';
-require '../Firebase-JWT/src/SignatureInvalidException.php';
-require '../Firebase-JWT/src/BeforeValidException.php';
-require '../Firebase-JWT/src/ExpiredException.php';
-
 $user = new User();
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
@@ -28,45 +19,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        // Verify token
-        $jwt = $_SERVER["HTTP_AUTHORIZATION"];
+        $id = $_GET['design_id'];
+        // fetch single files
+        $images = $user->fetchOneImage($id);
 
-        if (!$jwt) {
-            echo json_encode(array('success' => false, 'code' => 401, 'data' => array('message' => 'Unauthorized access')));
-            exit();
-        }
-
-        // Strip "Bearer " from the token
-        $jwt = str_replace("Bearer ", "", $jwt);
-
-        // Decode the token
-        try {
-            $decoded = JWT::decode($jwt, new Key($_ENV['KEY'], 'HS256'));
-            // echo json_encode(array('success' => false, 'code' => 401, 'data' => array('message' => 'Invalid', 'token' => $decoded)));
-
-            // Check if the token has expired
-            if ($decoded->exp < time()) {
-                echo json_encode(array('success' => false, 'code' => 401, 'data' => array('message' => 'Token has expired')));
-                exit();
-            }else {
-                $id = $_GET['design_id'];
-                // fetch single files
-                $images = $user->fetchOneImage($id);
-
-                if ($images) {
-                    $imageData = [];
-                    foreach ($images as $image) {
-                        $imageData[] = $image;
-                    }
-                    echo json_encode(array('success' => true, 'code' => 200, 'data' => array('message' => 'Image fetched successfully', 'image' => $imageData)));
-                } else {
-                    echo json_encode(array('success' => false, 'code' => 400, 'data' => array('message' => 'No image found')));
-                    exit();
-                }
+        if ($images) {
+            $imageData = [];
+            foreach ($images as $image) {
+                $imageData[] = $image;
             }
-        } catch (Exception $e) {
-            error_log($e->getMessage());
-            echo json_encode(array('success' => false, 'code' => 401, 'data' => array('message' => 'Invalid Token')));
+            echo json_encode(array('success' => true, 'code' => 200, 'data' => array('message' => 'Image fetched successfully', 'image' => $imageData)));
+        } else {
+            echo json_encode(array('success' => false, 'code' => 400, 'data' => array('message' => 'No image found')));
             exit();
         }
         break;
