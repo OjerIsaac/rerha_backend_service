b5dbf6dffa008e17efe49f8f6422bb61a9b49897
Ojer Isaac <isaacojerumu@gmail.com>
2023-03-11 04:14:11 +0100

    refactor: fetch single

--- admin/fetch_single.php
+++ admin/fetch_single.php
@@ -22,40 +22,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        $id = $_GET['design_id'];
-        // fetch single files
-        $images = $user->fetchOneImage($id);
+        $required_fields = array('design_id');
+        $empty_fields = array();
 
-        if ($images) {
-            foreach ($images as $image) {
-                $newImageData = array(
-                    "id" => $image["id"],
-                    "file_name" => $image["file_name"],
-                    "name" => $image["name"],
-                    "design_id" => $image["design_id"],
-                    "top" => $image["top"],
-                    "left_side" => $image["left_side"],
-                    "width" => $image["width"],
-                    "border" => $image["border"],
-                    "border_raduis_top_right" => $image["border_raduis_top_right"],
-                    "border_raduis_top_left" => $image["border_raduis_top_left"],
-                    "border_raduis_bottom_right" => $image["border_raduis_bottom_right"],
-                    "border_raduis_bottom_left" => $image["border_raduis_bottom_left"],
-                    "height" => $image["height"],
-                    "border_color" => $image["border_color"],
-                    "name_top" => $image["name_top"],
-                    "name_left" => $image["name_left"],
-                    "font_size" => $image["font_size"],
-                    "font_weight" => $image["font_weight"],
-                    "font_color" => $image["font_color"],
-                    "date" => $image["date"]
-                );
-                $imageData[] = $newImageData;
+        foreach ($required_fields as $field) {
+            if (empty($_REQUEST[$field])) {
+                $empty_fields[] = $field;
             }
-            echo json_encode(array('success' => true, 'code' => 200, 'data' => array('message' => 'Image fetched successfully', 'image' => $imageData)));
-        } else {
-            echo json_encode(array('success' => false, 'code' => 400, 'data' => array('message' => 'No image found')));
+        }
+        if (!empty($empty_fields)) {
+            echo json_encode(array('success' => false, 'code' => 400, 'data' => array('message' => 'This fields ' . implode(', ', $empty_fields) . ' cannot be empty')));
             exit();
+        } else {
+            $id = $_REQUEST['design_id'];
+            // fetch single files
+            $images = $user->fetchOneImage($id);
+
+            if ($images) {
+                foreach ($images as $image) {
+                    $newImageData = array(
+                        "id" => $image["id"],
+                        "file_name" => $image["file_name"],
+                        "name" => $image["name"],
+                        "design_id" => $image["design_id"],
+                        "top" => $image["top"],
+                        "left_side" => $image["left_side"],
+                        "width" => $image["width"],
+                        "border" => $image["border"],
+                        "border_raduis_top_right" => $image["border_raduis_top_right"],
+                        "border_raduis_top_left" => $image["border_raduis_top_left"],
+                        "border_raduis_bottom_right" => $image["border_raduis_bottom_right"],
+                        "border_raduis_bottom_left" => $image["border_raduis_bottom_left"],
+                        "height" => $image["height"],
+                        "border_color" => $image["border_color"],
+                        "name_top" => $image["name_top"],
+                        "name_left" => $image["name_left"],
+                        "font_size" => $image["font_size"],
+                        "font_weight" => $image["font_weight"],
+                        "font_color" => $image["font_color"],
+                        "date" => $image["date"]
+                    );
+                    $imageData[] = $newImageData;
+                }
+                echo json_encode(array('success' => true, 'code' => 200, 'data' => array('message' => 'Image fetched successfully', 'image' => $imageData)));
+            } else {
+                echo json_encode(array('success' => false, 'code' => 400, 'data' => array('message' => 'No image found')));
+                exit();
+            }
         }
         break;
     case 'POST':
