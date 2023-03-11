a01930c12167ee09352f83258a1718c716b17898
Ojer Isaac <isaacojerumu@gmail.com>
2023-03-11 10:11:24 +0100

    chores: cors

--- admin/fetch_single.php
+++ admin/fetch_single.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Origin: https://rerha-leyjhy6ro-promiseejiro.vercel.app");
+header("Access-Control-Allow-Origin: https://rerhadp.vercel.app");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Content-Type: application/json; charset=UTF-8");
--- return.php
+++ return.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Origin: https://rerha-leyjhy6ro-promiseejiro.vercel.app");
+header("Access-Control-Allow-Origin: https://rerhadp.vercel.app");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header('Content-Type: application/json');
